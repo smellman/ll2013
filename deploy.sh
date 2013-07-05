a07e3b738c02aae0e10a12055580a9c9a9395b56
Taro Matsuzawa <taro@georepublic.co.jp>
2013-07-05 23:38:02 +0900

    bundleをつかうようにした

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ fi
 
 if [ $1 = "jus" ]; then
   echo "deploy to ll.jus.or.jp/2013"
-  jekyll --base-url http://ll.jus.or.jp/2013 ../deploy/ll.jus
+  bundle exec jekyll --base-url http://ll.jus.or.jp/2013 ../deploy/ll.jus
   rsync -av ../deploy/ll.jus/ ll.jus.or.jp:/usr/local/docs/ll.jus.or.jp/llweb/2013
   exit 1
 fi
 
 if [ $1 = "smellman" ]; then
   echo "deploy to ll2013.smellman.org"
-  jekyll --base-url http://ll2013.smellman.org ../deploy/smellman.org
+  bundle exec jekyll --base-url http://ll2013.smellman.org ../deploy/smellman.org
   rsync -av ../deploy/smellman.org/ synthamesk:/home/nginx/ll2013
   exit 1
 fi
